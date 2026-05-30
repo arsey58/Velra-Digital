59e3bb206859945a5093eae18eaede6cbabcc9e6
arsey58 <57042822+arsey58@users.noreply.github.com>
2026-05-31 00:52:32 +0500

    fixes. 2.2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
-export default defineConfig(() => {
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+        chunkFileNames: `assets/[name]-[hash]-${Date.now()}.js`,
+        assetFileNames: `assets/[name]-[hash]-${Date.now()}.[ext]`
+      }
+    }
+  },  
   return {
     plugins: [react(), tailwindcss()],
     resolve: {
